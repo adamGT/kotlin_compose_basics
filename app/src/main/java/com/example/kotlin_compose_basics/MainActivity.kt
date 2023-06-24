2f0b73f8d87805be801ba93aaad2ed6cc1441f7d
adamGT <adamgetachew55@gmail.com>
2023-06-24 16:52:40 +0300

    added the message card component using composable

--- app/src/main/java/com/example/kotlin_compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_compose_basics/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.kotlin_compose_basics
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlin_compose_basics.ui.theme.Kotlin_compose_basicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +28,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Kotlin_compose_basicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MessageCard(Message("Adam G", "is awesome"))
+            }
+        }
+    }
+}
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg : Message){
+    Row {
+        Image(painter = painterResource(id = R.drawable.lion),
+            contentDescription = "Lion Profile Image",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+
+        )
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column() {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode")
 @Composable
-fun GreetingPreview() {
-    Kotlin_compose_basicsTheme {
-        Greeting("Android")
+fun ReviewAllView(){
+    Kotlin_compose_basicsTheme{
+        Surface {
+            MessageCard(Message("Adam G", "Hi there bro what are we doing next week"))
+        }
     }
 }
